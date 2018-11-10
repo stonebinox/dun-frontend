8563151c2888e4cc307e02fe7a6fe545c6deccec
Anoop Santhanam <anoop.santhanam@gmail.com>
2018-11-10 09:04:42 +0530

    Updated paypal button

--- src/RequestForm.js
+++ src/RequestForm.js
@@ -171,10 +171,10 @@ export class RequestForm extends Component {
                             <div style={{display: this.state.payDisplay}} className="text-center">
                                 <hr/>
                                 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                                    <input type="hidden" name="cmd" value="_s-xclick"/>
-                                    <input type="hidden" name="hosted_button_id" value="HUL837WKYWJYL"/>
-                                    <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!"/>
-                                    <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1"/>
+                                    <input type="hidden" name="cmd" value="_s-xclick" />
+                                    <input type="hidden" name="hosted_button_id" value="HUL837WKYWJYL" />
+                                    <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!" />
+                                    <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
                                 </form>
                             </div>
                         </div>
